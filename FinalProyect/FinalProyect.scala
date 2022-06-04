e84e3bb9c8708f5178827aa70bb989ee38dbc25a
Manuel <manuel.real18@tectijuana.edu.mx>
2022-06-04 00:19:50 -0700

    Transform Raw Data Config

--- FinalProyect/FinalProyect.scala
+++ FinalProyect/FinalProyect.scala
@@ -1,3 +1,11 @@
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.VectorAssembler
 
-val df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("E:/Programas/Git/BigData/FinalProyect/bank-full.csv")
+val df = spark.read.format("csv").option("sep", ";").option("header", "true").option("inferSchema", "true").load("E:/Programas/Git/BigData/FinalProyect/bank-full.csv")
+
+df.printSchema()
+
+df.show(15)
